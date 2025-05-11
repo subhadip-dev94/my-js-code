30c0d659e386860e53ce76b008dbdd8963261773
subhadip-dev94 <s.maity.developer@gmail.com>
2025-05-11 12:36:41 +0530

    Update array-2d.js

--- array-2d.js
+++ array-2d.js
@@ -1,1 +1,15 @@
+// an example where the goal is to count the occurrences of distinct elements in a 2D array of strings.using flat() and reduce()
 
+const input = [
+  ["a", "b", "c"],
+  ["c", "d", "f"],
+  ["d", "f", "g"],
+];
+
+const flatArr  =  input.flat();
+const result = flatArr.reduce((acc, curr) => {
+  acc[curr] = (acc[curr] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(result);
